9f4624a87503a2d684a112336da211cd262713f6
dennisnyawira <gitutonyawira@gmail.com>
2024-01-21 21:47:38 +0300

    Test Data Generation using Java-Faker. This dependency has been added on Maven

--- src/main/java/testdata/GenerateTestData.java
+++ src/main/java/testdata/GenerateTestData.java
@@ -1,4 +1,49 @@
 package testdata;
 
+import com.github.javafaker.Faker;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class GenerateTestData {
+
+    public static class CreateBookingData {
+        private final Faker faker;
+        private final SimpleDateFormat dateFormat;
+
+
+        public CreateBookingData() {
+            dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            faker = new Faker();
+        }
+
+        public String firstName() {
+            return faker.name().firstName();
+        }
+
+        public String lastName() {
+            return faker.name().lastName();
+        }
+
+        public boolean isDepositPaid() {
+            return faker.bool().bool(); // Returns Random True or False.
+        }
+
+        public String getCheckingDate() {
+            Date date = faker.date().future(365, TimeUnit.DAYS);
+            return dateFormat.format(date);
+        }
+
+        public String getCheckOUtDate() {
+            Date date = faker.date().future(365, TimeUnit.DAYS);
+            return dateFormat.format(date);
+        }
+
+        public String additionalDetails() {
+            return faker.lorem().sentence();
+        }
+    }
+
+
 }
--- src/test/java/unittests/GenerateTestDataTests.java
+++ src/test/java/unittests/GenerateTestDataTests.java
@@ -1,4 +1,64 @@
 package unittests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import testdata.GenerateTestData;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class GenerateTestDataTests {
+    private  GenerateTestData.CreateBookingData createBookingData;
+    private SimpleDateFormat dateFormat;
+
+    @BeforeMethod
+    public void setup()
+    {
+        createBookingData =  new GenerateTestData.CreateBookingData();
+        dateFormat =  new SimpleDateFormat("yyyy-MM-dd");
+    }
+    @Test
+    void testFirstName() {
+        String firstName = createBookingData.firstName();
+        Assert.assertNotNull(firstName);
+    }
+    @Test
+    void testLastName ()
+    {
+        String lastName = createBookingData.lastName();
+        Assert.assertNotNull(lastName);
+    }
+    @Test
+    void testIsDepositPaid()
+    {
+        Boolean isDepositPaid =  createBookingData.isDepositPaid();
+        Assert.assertNotNull(isDepositPaid);
+    }
+    @Test
+    void testAdditionalDetails()
+    {
+        String additionalDetails = createBookingData.additionalDetails();
+        Assert.assertNotNull(additionalDetails);
+        System.out.println(additionalDetails);
+    }
+    @Test
+    public void testCheckInDateIsFormattedCorrectly() throws ParseException {
+        String checkInDate = createBookingData.getCheckingDate();
+        Date date = dateFormat.parse(checkInDate);
+        String formattedDate = dateFormat.format(date);
+        Assert.assertEquals(checkInDate, formattedDate, "Check-in date should be formatted as yyyy-MM-dd");
+        System.out.println(checkInDate);
+    }
+    @Test
+    public void testCheckOUtDateIsFormattedCorrectly() throws ParseException {
+        String checkOutDate = createBookingData.getCheckOUtDate();
+        Date date =  dateFormat.parse(checkOutDate);
+        String formattedDate = dateFormat.format(date);
+        Assert.assertEquals(checkOutDate, formattedDate,"Check-out date should be formatted as yyyy-MM-dd");
+        System.out.println(checkOutDate);
+    }
+
 }
+
