846462f660958eedeb14d30e5eb7d9f46ea9fd24
dennisnyawira <gitutonyawira@gmail.com>
2024-01-24 22:23:05 +0300

    Adding the Test Class

--- src/test/java/services/CreateBookingTests.java
+++ src/test/java/services/CreateBookingTests.java
@@ -1,4 +1,27 @@
 package services;
 
+import clients.BookingClient;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Properties;
+
 public class CreateBookingTests {
+
+    private BookingClient bookingClient;
+    private Response response;
+    @BeforeMethod
+    private void setup()
+    {
+        bookingClient =  new BookingClient();
+    }
+
+    @Test
+    public void successfulCreation()
+    {
+        response = bookingClient.createBooking();
+        Assert.assertEquals(200, 200);
+    }
 }
